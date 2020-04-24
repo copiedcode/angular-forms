aaac5f11ccc476143d2e295baf15ab3fcaf6314b
copiedcode <raphael.webdesign@outlook.com>
2020-04-24 18:18:43 +0200

    add error message to name field

--- src/app/user-settings-form/user-settings-form.component.ts
+++ src/app/user-settings-form/user-settings-form.component.ts
@@ -9,11 +9,11 @@ import { UserSettings } from '../data/user-settings';
 export class UserSettingsFormComponent implements OnInit {
 
   originalUserSettings: UserSettings = {
-    name: 'Name',
-    emailOffers: true,
-    interfaceStyle: 'dark',
-    subscriptionType: 'Jährlich',
-    notes: 'here are some notes...'
+    name: null,
+    emailOffers: null,
+    interfaceStyle: null,
+    subscriptionType: null,
+    notes: null
   };
 
   userSettings : UserSettings = { ...this.originalUserSettings };
