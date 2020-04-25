7c3a407d8daca61f14f8d4d590b7002fd900424f
copiedcode <raphael.webdesign@outlook.com>
2020-04-25 10:04:42 +0200

    setup http access

--- src/app/user-settings-form/user-settings-form.component.ts
+++ src/app/user-settings-form/user-settings-form.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UserSettings } from '../data/user-settings';
 import { NgForm, NgModel } from '@angular/forms';
+import { DataService } from '../data/data.service';
 
 @Component({
   selector: 'app-user-settings-form',
@@ -19,13 +20,17 @@ export class UserSettingsFormComponent implements OnInit {
 
   userSettings : UserSettings = { ...this.originalUserSettings };
   
-  constructor() { }
+  constructor(private dataService: DataService) { }
 
   ngOnInit(): void {
   }
 
   onSubmit(form: NgForm) {
     console.log("in onsubmit", form.valid);
+    this.dataService.postUserSettingsForm(this.userSettings).subscribe(
+      result => console.log('success', result),
+      error => console.log('error', error)
+    );
   }
 
   onBlur(field: NgModel){
